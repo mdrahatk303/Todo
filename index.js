2f100e99efd9d87279dc5add8803951a94c9619e
Md Rahat Khan <mdrahatk303@gmail.com>
2019-10-28 15:32:40 +0530

    setting view engine

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port=8000;
 
 //using express router
 app.use('/',require('./routes'));
+
+//setting view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err)
 {
     if(err)
